a94b3416e87f0292757f345d87a4d2ade75af4f5
Przemyslaw Madej <przemyslaw.madej@yahoo.pl>
2013-11-17 15:15:33 +0100

    start

---
+++ WMH/Model/Edge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMH.Model
+{
+    public class Edge
+    {
+        public Vertex Start { get; private set; }
+        public Vertex End { get; private set; }
+
+        public double Length
+        {
+            get
+            {
+                return Math.Sqrt(Math.Pow(this.End.X - this.Start.Y, 2) + Math.Pow(this.End.X - this.Start.Y, 2));
+            }
+        }
+
+        public Edge(Vertex start, Vertex end)
+        {
+            this.Start = start;
+            this.End = end;
+        }
+    }
+}
